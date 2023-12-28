df38fa08ce8ee6b570bc99e0343618d9e894d82a
Denys Bzenko <dbzenko@kse.org.ua>
2023-12-28 23:07:52 +0200

    Add button back

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import telebot
 from telebot import types
 
-# Токен бота
+
 TOKEN = "6378053363:AAFDlqyTZqpKvqtn5zXhzHT3uJDZXHjtyiQ"
 bot = telebot.TeleBot(TOKEN)
 
-# Зберігання даних користувача
+
 user_data = {}
 
-# Послідовність кроків
+
 STEPS = ['district', 'room', 'area', 'budget']
 
-# Функція для отримання попереднього кроку
+
 def get_prev_step(chat_id):
     current_index = STEPS.index(user_data[chat_id]['current_step'])
     return STEPS[max(0, current_index - 1)]
 
-# Функція для обробки вибору користувача
+
 def handle_choice(chat_id, data, message_id):
     current_step = user_data[chat_id]['current_step']
     user_data[chat_id][current_step] = data.split('_')[1]
@@ -28,7 +28,7 @@ def handle_choice(chat_id, data, message_id):
         user_data[chat_id]['current_step'] = next_step
         bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"Вибрано {data.split('_')[1]}. Ваш наступний вибір:", reply_markup=get_keyboard(next_step))
     else:
-        # Всі вибори зроблено, виведення результатів
+
         summary = "\n".join([f"{key}: {value}" for key, value in user_data[chat_id].items() if key != 'current_step'])
         bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=f"Фільтрація завершена! Ось ваші вибори:\n{summary}")
 
@@ -39,7 +39,7 @@ def handle_query(call):
     data = call.data
 
     if data == 'back':
-        # Користувач натиснув кнопку "Назад"
+
         prev_step = get_prev_step(chat_id)
         user_data[chat_id]['current_step'] = prev_step
         bot.edit_message_text(chat_id=chat_id, message_id=call.message.message_id, text=f"Повертаємось на крок: {prev_step}", reply_markup=get_keyboard(prev_step))
@@ -47,14 +47,13 @@ def handle_query(call):
         # Обробка вибору користувача
         handle_choice(chat_id, data, call.message.message_id)
 
-# Вітальне повідомлення
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     chat_id = message.chat.id
     user_data[chat_id] = {'current_step': 'district'}
     bot.send_message(chat_id, "Привіт! Почнемо вибір району.", reply_markup=create_district_keyboard())
 
-# Функції для створення клавіатур з кнопками
 def create_district_keyboard():
     keyboard = types.InlineKeyboardMarkup()
     districts = ["Голосіївський", "Печерський", "Шевченківський", "Соломʼянський", "Подільський", "Дарницький", "Дніпровський", "Оболонський", "Святошинський"]
@@ -83,23 +82,27 @@ def create_budget_keyboard():
     keyboard.add(*buttons)
     return keyboard
 
-# Функція для отримання клавіатури залежно від кроку
+
 def get_keyboard(step):
     keyboard = types.InlineKeyboardMarkup()
-    # Додати кнопку "Назад" (крім першого кроку)
+
+
     if step != 'district':
         keyboard.add(types.InlineKeyboardButton(text="Назад", callback_data="back"))
-    # Визначення клавіатур для кожного кроку
+
+
     if step == 'district':
-        return create_district_keyboard()
+        return keyboard.add(*[types.InlineKeyboardButton(text=d, callback_data=f'district_{d}') for d in ["Голосіївський", "Печерський", "Шевченківський", "Соломʼянський", "Подільський", "Дарницький", "Дніпровський", "Оболонський", "Святошинський"]])
     elif step == 'room':
-        return create_room_keyboard()
+        return keyboard.add(*[types.InlineKeyboardButton(text=r, callback_data=f'room_{r}') for r in ["1-кімнатна", "2-кімнатна", "3-кімнатна", "4-кімнатна", "4+ -кімнатна"]])
     elif step == 'area':
-        return create_area_keyboard()
+        return keyboard.add(*[types.InlineKeyboardButton(text=a, callback_data=f'area_{a}') for a in ["до 30", "до 40", "до 50", "до 70", "до 90", "до 110", "до 130", "до 150", "до 170", "до 190", "від 200"]])
     elif step == 'budget':
-        return create_budget_keyboard()
+        return keyboard.add(*[types.InlineKeyboardButton(text=b, callback_data=f'budget_{b}') for b in ["до 800$", "до 900$", "до 1000$", "до 1100$", "до 1200$", "до 1300$", "до 1400$", "до 1500$", "до 1700$", "до 1900$", "до 2100$", "до 2300$", "до 2500$", "до 3000$", "від 3000$"]])
+
     return keyboard
 
-# Запуск бота
+
+
 if __name__ == '__main__':
     bot.polling(none_stop=True)
